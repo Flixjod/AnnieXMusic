7a138c9124d61610472dd342bec1a204feef7150
Flixjod <144277345+Flixjod@users.noreply.github.com>
2025-09-16 17:11:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Siesta_botz")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SiestaDiscussion")
 
 # ───── Assistant Auto Leave ───── #
-AUTO_LEAVING_ASSISTANT = True
+AUTO_LEAVING_ASSISTANT = False
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "11500"))
 
 # ───── Error Handling ───── #
