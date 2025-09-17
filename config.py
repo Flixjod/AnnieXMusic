3afd7dfe8c34216a20eb9faa3cafd54c9b0f57a5
Flixjod <144277345+Flixjod@users.noreply.github.com>
2025-09-17 11:10:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ───── Git & Updates ───── #
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/CertifiedCoders/AnnieXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Flixjod/AnnieXMusic/tree/Master")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
