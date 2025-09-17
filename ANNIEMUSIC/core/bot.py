8a23fb23fe8e0a765db20cd4cb7b0b8cb0d91145
Flixjod <144277345+Flixjod@users.noreply.github.com>
2025-09-17 19:18:55 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -11,7 +11,7 @@ from ..logging import LOGGER
 class FLiX(Client):
     def __init__(self):
         super().__init__(
-            name="AnnieXMusic",
+            name="SiestaXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
